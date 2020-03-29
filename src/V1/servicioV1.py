60dff9ef65e3d25ef9e08852e93639c2e2dcd43e
Miguel Jiménez Cazorla <imiguel10@hotmail.es>
2020-03-29 13:13:59 +0200

    Host como variable de entorno

--- src/V1/servicioV1.py
+++ src/V1/servicioV1.py
@@ -7,11 +7,14 @@ import json
 hours = ["00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00",
 "11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00",
 "23:00"]
+
+host = os.environ["HOST"]
+
 class ServiceForecasting:
     
     def __init__(self):
         # Generamos el dataframe con 1000 entradas de la base de datos
-        self.engine = create_engine('mysql+pymysql://miguel:miguel@db/forecast')
+        self.engine = create_engine('mysql+pymysql://miguel:miguel@'+host+'/forecast')
         self.df = self.engine.execute("SELECT * FROM forecast LIMIT 1000").fetchall()
         self.df = pd.DataFrame(data=self.df)
 
